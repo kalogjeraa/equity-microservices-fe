5b35676418a57df1259150d1dc749d7a9206624c
guinevere <kalogjeraa@gmail.com>
2021-01-22 23:33:35 -0500

    Delete unnecessary boilerplate files, import react-router-dom, create Routes.js and Home.js.

---
+++ src/components/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+
+const Routes = () => {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} exact />
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+export default Routes;
